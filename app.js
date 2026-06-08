c40673c77dd8ba168606d40ea0cb38ec8af2d837
zoe-code2026 <zoeli@tri-win.com>
2026-06-08 14:43:03 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ async function renderLockedTemplate() {
 
 async function loadActivities() {
   try {
-    const response = await fetch("./activities.json");
+    const response = await fetch("./activities.json?v=20260605-2");
 
     if (!response.ok) {
       throw new Error("Could not load activities.json");
